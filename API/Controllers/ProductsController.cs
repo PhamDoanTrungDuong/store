1a09b773801ea14161d936f68a0b96bac7857f38
thienanga075 <pdtduong@gmail.com>
2022-06-26 19:48:49 +0700

    Mess "Basic API"

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+     public class ProductsController : BaseController
+     {
+          private readonly StoreContext _context;
+          public ProductsController(StoreContext context)
+          {
+                _context = context;
+          }
+
+          [HttpGet]
+          public async Task<ActionResult<List<Product>>> GetProducts(){
+                var products = await _context.Products.ToListAsync();
+                return Ok(products);
+          }
+
+          [HttpGet("{id}")]
+          public async Task<ActionResult<Product>> GetProductById(int id){
+            return await _context.Products.FindAsync(id);
+          }
+     }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                Console.WriteLine("Seeding database...");
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
