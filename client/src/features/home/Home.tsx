6381b88585121ca4ea21135f775c455ddd2592d2
thienanga075 <pdtduong@gmail.com>
2022-08-11 19:16:29 +0700

    deploy

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -38,7 +38,21 @@ const Home: React.FC = () => {
 		return () => {
 			dispatch(setStateUser());
 		};
-	});
+	}, [dispatch, status]);
+
+	// useEffect(() => {
+	// 	if (status === "logoutSuccess") {
+	// 		Swal.fire({
+	// 			icon: "warning",
+	// 			title: "Your has been Logout",
+	// 			showConfirmButton: false,
+	// 			timer: 1500,
+	// 		});
+	// 	}
+	// 	return () => {
+	// 		dispatch(setStateUser());
+	// 	};
+	// }, [dispatch, status]);
 
 	const { user } = useAppSelector((state) => state.account);
 	return (
